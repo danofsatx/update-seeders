4ed6f8215e308fc8894bafe65a8e55ca0d711642
Dan Mossor <dmossor@fedoraproject.org>
2015-09-12 20:46:22 -0500

    Just a script for the ISOs - Kicks off download of all ISOs in parallel

---
+++ update-isos.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+#######################################################
+# This is intended to be tacked on to the end of the 
+# tracker updater script. All variables are declared 
+# here to run as a stand-alone, however. To add to the
+# main script, remove these variables as they are already
+# declared.
+
+alt_fed='http://alt.fedoraproject.org/pub/alt/live-respins/'
+#release=$(curl ${alt_fed} |awk '{print $6}'|awk '{print substr($0,7,3)}'|sed '15q;d') &> /dev/null
+#updated=$(curl ${alt_fed}|awk '{print $7}'|sed '15q;d'|sed -e 's/-//g') &> /dev/null
+release="F22"
+updated="20150912"
+
+# Torrent variables
+desk32tfile="$release-i686-DESK-$updated.torrent"
+desk64tfile="$release-x86_64-DESK-$updated.torrent"
+kde32tfile="$release-i686-KDE-$updated.torrent"
+kde64tfile="$release-x86_64-KDE-$updated.torrent"
+lxde32tfile="$release-i686-LXDE-$updated.torrent"
+lxde64tfile="$release-x86_64-LXDE-$updated.torrent"
+mate32tfile="$release-i686-MATE-$updated.torrent"
+mate64tfile="$release-x86_64-MATE-$updated.torrent"
+soas32tfile="$release-i686-SOAS-$updated.torrent"
+soas64tfile="$release-x86_64-SOAS-$updated.torrent"
+xfce32tfile="$release-i686-XFCE-$updated.torrent"
+xfce64tfile="$release-x86_64-XFCE-$updated.torrent"
+
+# ISO  variables
+desk32ifile="$release-i686-DESK-$updated.iso"
+desk64ifile="$release-x86_64-DESK-$updated.iso"
+kde32tiile="$release-i686-KDE-$updated.iso"
+kde64tiile="$release-x86_64-KDE-$updated.iso"
+lxde32ifile="$release-i686-LXDE-$updated.iso"
+lxde64ifile="$release-x86_64-LXDE-$updated.iso"
+mate32ifile="$release-i686-MATE-$updated.iso"
+mate64ifile="$release-x86_64-MATE-$updated.iso"
+soas32ifile="$release-i686-SOAS-$updated.iso"
+soas64ifile="$release-x86_64-SOAS-$updated.iso"
+xfce32ifile="$release-i686-XFCE-$updated.iso"
+xfce64ifile="$release-x86_64-XFCE-$updated.iso"
+
+# The seeder urls
+seeder_1='http://localhost:9091/transmission/rpc --auth=respins:FedoraLiveISOs'
+#seeder_2='http://seeder2.url/transmission/rpm --auth=user:pass'
+#seeder_3='http://seeder3.url/transmission/rpm --auth=user:pass'
+#seeder_4='http://seeder4.url/transmission/rpm --auth=user:pass'
+
+# Here we go...
+echo "Downloading ISOs...."
+
+ISO="$desk32ifile $desk64ifile $kde32tiile $kde64tiile $lxde32ifile $lxde64ifile $mate32ifile $mate64ifile $soas32ifile $soas64ifile $xfce32ifile $xfce64ifile"
+
+set -m
+
+for i in $ISO; 
+        do wget --quiet $alt_fed$i -P /home/torrents/ &
+done
+while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
+
+#for i in $ISO;
+#        do chown transmission:transmission /srv/torrents/$i 
+#done
+
+echo "The ISOs should be downloaded."
+echo "Please verify before continuing."
+#wait
+
+#echo "Populating the seeders..."
+
+# Start a loop
+#for seed in "$seeder_1" "$seeder_2" "$seeder_3" "$seeder_4"; do
+        echo "Updating $seed with $release-$updated"
+#       transmission-remote $seed -R
+#        for tfile in "$desk32tfile" "$desk64tfile" "$kde32tfile" "$kde64tfile" "$lxde32tfile" "$lxde64tfile" "$mate32tfile" "$mate64tfile" "$soas32tfile" "$soas64tfile" "$xfce32tfile" "$xfce64tfile"; do
+#                transmission-remote $seeder_1 -a $alt_fed$tfile
+#        done
+#done
+
+
