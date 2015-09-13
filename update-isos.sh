773bcf81486e2f48c5af54c868cdeeea9745c2bb
Dan Mossor <dmossor@fedoraproject.org>
2015-09-12 21:01:34 -0500

    Cleaned up ISO updater file to get all the other cruft out

--- update-isos.sh
+++ update-isos.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 #######################################################
-# This is intended to be tacked on to the end of the 
-# tracker updater script. All variables are declared 
-# here to run as a stand-alone, however. To add to the
-# main script, remove these variables as they are already
-# declared.
+# This script is just to download the updated Live ISOs
+# from dl.fp.o/alt. It is currently designed to D/L all
+# of the ISOs in parallel, but it gives zero progress
+# reports (that's a TODO). It will also grab the 
+# .torrent files in order to populate a local seeder.
+
+# Set this line according to your local configuration -
+# this is where the ISO and .torrent files will go.
+destDir="/home/torrents/"
 
 alt_fed='http://alt.fedoraproject.org/pub/alt/live-respins/'
 #release=$(curl ${alt_fed} |awk '{print $6}'|awk '{print substr($0,7,3)}'|sed '15q;d') &> /dev/null
@@ -41,41 +45,27 @@ soas64ifile="$release-x86_64-SOAS-$updated.iso"
 xfce32ifile="$release-i686-XFCE-$updated.iso"
 xfce64ifile="$release-x86_64-XFCE-$updated.iso"
 
-# The seeder urls
-seeder_1='http://localhost:9091/transmission/rpc --auth=respins:FedoraLiveISOs'
-#seeder_2='http://seeder2.url/transmission/rpm --auth=user:pass'
-#seeder_3='http://seeder3.url/transmission/rpm --auth=user:pass'
-#seeder_4='http://seeder4.url/transmission/rpm --auth=user:pass'
-
 # Here we go...
 echo "Downloading ISOs...."
 
 ISO="$desk32ifile $desk64ifile $kde32tiile $kde64tiile $lxde32ifile $lxde64ifile $mate32ifile $mate64ifile $soas32ifile $soas64ifile $xfce32ifile $xfce64ifile"
 
+# this line is what turns the threading on, -m is default off in bash
 set -m
 
+# This is the loop that gets threaded
 for i in $ISO; 
-        do wget --quiet $alt_fed$i -P /home/torrents/ &
+        do wget --quiet $alt_fed$i -P $destDir &
 done
+
+# And this is the line that splits it all up for threading.
 while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
 
-#for i in $ISO;
-#        do chown transmission:transmission /srv/torrents/$i 
-#done
-
-echo "The ISOs should be downloaded."
-echo "Please verify before continuing."
-#wait
-
-#echo "Populating the seeders..."
+for i in $ISO;
+        do chown transmission:transmission $destDir$i 
+done
 
 # Start a loop
-#for seed in "$seeder_1" "$seeder_2" "$seeder_3" "$seeder_4"; do
-        echo "Updating $seed with $release-$updated"
-#       transmission-remote $seed -R
-#        for tfile in "$desk32tfile" "$desk64tfile" "$kde32tfile" "$kde64tfile" "$lxde32tfile" "$lxde64tfile" "$mate32tfile" "$mate64tfile" "$soas32tfile" "$soas64tfile" "$xfce32tfile" "$xfce64tfile"; do
-#                transmission-remote $seeder_1 -a $alt_fed$tfile
-#        done
-#done
-
-
+for tfile in "$desk32tfile" "$desk64tfile" "$kde32tfile" "$kde64tfile" "$lxde32tfile" "$lxde64tfile" "$mate32tfile" "$mate64tfile" "$soas32tfile" "$soas64tfile" "$xfce32tfile" "$xfce64tfile";
+	do wget --quiet $alt_fed$tfile -P $destDir
+done
